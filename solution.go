8b87757627d9b7ae3c1bbee7d95207f7da259116
MilDeRRy <bane009@yandex.ru>
2022-06-10 15:19:47 +0400

    task 00

--- solution.go
+++ solution.go
@@ -1,5 +1,16 @@
-package solution
+package main
 
-func GetMessage() string {
-	return "I'm beaver!"
+import (
+	"fmt"
+
+	"github.com/kyokomi/emoji/v2"
+)
+
+func main() {
+	//fmt.Println("Hello :world_map:!")
+
+	//emoji.Println(":beer: Beer!!!")
+
+	pizzaMessage := emoji.Sprint("Hello :world_map:!")
+	fmt.Println(pizzaMessage)
 }
